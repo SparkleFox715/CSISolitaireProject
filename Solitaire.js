f77dcec1d10833d9b2e81333fef341c40ce46890
DanielC12321 <C0903178@students.katyisd.org>
2022-02-02 07:51:01 -0600

    update

--- Solitaire.js
+++ Solitaire.js
@@ -1,1 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('new_game').addEventListener('click', () => {
+        start(); 
+    });
+});
+
+function start(){
+    const response = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle");
+    let jsonData = JSON.parse(response);
+    console.log(jsonData);
+
+
+
+    
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
 
