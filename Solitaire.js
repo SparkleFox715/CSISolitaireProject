493091a5abb56c95ad0a71a862eab7658bad6679
DanielC12321 <C0903178@students.katyisd.org>
2022-02-07 08:04:57 -0600

    update

--- Solitaire.js
+++ Solitaire.js
@@ -4,17 +4,26 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-function start(){
-    const response = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle");
-    let jsonData = JSON.parse(response);
-    console.log(jsonData);
+async function start(){
+    var resp;
+    let fetchRes = fetch(
+        "https://deckofcardsapi.com/api/deck/new/shuffle");
+          
+                // fetchRes is the promise to resolve
+                // it by using.then() method
+                fetchRes.then(res =>
+                    res.json()).then(d => {
 
+                        console.log(d);
+                        $.each(d, function() {
+                            var key = Object.keys(this)[0];
+                            var value = this[key];
+                            //do something with value;
+                          }); 
 
-
-    
-
-
-
+                          console.log(d["deck_id"]);
+                    })
+               
         }
 
 
