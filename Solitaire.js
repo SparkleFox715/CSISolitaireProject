0b155cf6baace10c4aeb503f53d9337b6e95c541
DanielC12321 <C0903178@students.katyisd.org>
2022-02-08 07:51:20 -0600

    cards

--- Solitaire.js
+++ Solitaire.js
@@ -4,25 +4,119 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-async function start(){
-    var resp;
-    let fetchRes = fetch(
-        "https://deckofcardsapi.com/api/deck/new/shuffle");
-          
-                // fetchRes is the promise to resolve
-                // it by using.then() method
-                fetchRes.then(res =>
-                    res.json()).then(d => {
+function start(){
+    var deck1 = Array();
+    var deck2 = Array();
+    deck1.push("OC");
+    deck1.push("OD");
+    deck1.push("OH");
+    deck1.push("OS");
+    deck1.push("2C");
+    deck1.push("2D");
+    deck1.push("2S");
+    deck1.push("3C");
+    deck1.push("2H");
+    deck1.push("3D");
+    deck1.push("3H");
+    deck1.push("3S");
+    deck1.push("4C");
+    deck1.push("4D");
+    deck1.push("4H");
+    deck1.push("4S");
+    deck1.push("5C");
+    deck1.push("5D");
+    deck1.push("5H");
+    deck1.push("5S");
+    deck1.push("6C");
+    deck1.push("6D");
+    deck1.push("6H");
+    deck1.push("6S");
+    deck1.push("7C");
+    deck1.push("7D");
+    deck1.push("7H");
+    deck1.push("7S");
+    deck1.push("8C");
+    deck1.push("8D");
+    deck1.push("8H");
+    deck1.push("8S");
+    deck1.push("9C");
+    deck1.push("9D");
+    deck1.push("9H");
+    deck1.push("9S");
+    deck1.push("AC");
+    deck1.push("AD");
+    deck1.push("AH");
+    deck1.push("AS");
+    deck1.push("JC");
+    deck1.push("JD");
+    deck1.push("JH");
+    deck1.push("JS");
+    deck1.push("KC");
+    deck1.push("KD");
+    deck1.push("KH");
+    deck1.push("KS");
+    deck1.push("QC");
+    deck1.push("QD");
+    deck1.push("QH");
+    deck1.push("QS");
 
-                        console.log(d);
-                        $.each(d, function() {
-                            var key = Object.keys(this)[0];
-                            var value = this[key];
-                            //do something with value;
-                          }); 
 
-                          console.log(d["deck_id"]);
-                    })
+    deck2.push("OC");
+    deck2.push("OD");
+    deck2.push("OH");
+    deck2.push("OS");
+    deck2.push("2C");
+    deck2.push("2D");
+    deck2.push("2S");
+    deck2.push("3C");
+    deck2.push("2H");
+    deck2.push("3D");
+    deck2.push("3H");
+    deck2.push("3S");
+    deck2.push("4C");
+    deck2.push("4D");
+    deck2.push("4H");
+    deck2.push("4S");
+    deck2.push("5C");
+    deck2.push("5D");
+    deck2.push("5H");
+    deck2.push("5S");
+    deck2.push("6C");
+    deck2.push("6D");
+    deck2.push("6H");
+    deck2.push("6S");
+    deck2.push("7C");
+    deck2.push("7D");
+    deck2.push("7H");
+    deck2.push("7S");
+    deck2.push("8C");
+    deck2.push("8D");
+    deck2.push("8H");
+    deck2.push("8S");
+    deck2.push("9C");
+    deck2.push("9D");
+    deck2.push("9H");
+    deck2.push("9S");
+    deck2.push("AC");
+    deck2.push("AD");
+    deck2.push("AH");
+    deck2.push("AS");
+    deck2.push("JC");
+    deck2.push("JD");
+    deck2.push("JH");
+    deck2.push("JS");
+    deck2.push("KC");
+    deck2.push("KD");
+    deck2.push("KH");
+    deck2.push("KS");
+    deck2.push("QC");
+    deck2.push("QD");
+    deck2.push("QH");
+    deck2.push("QS");
+
+
+
+
                
         }
 
